2d80475eb07e0660251bb792ee7bc63ac6c0f105
its-justus <ianjohnson9042@gmail.com>
2020-06-25 12:08:41 -0500

    sagas query giphy works

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,16 +1,30 @@
 import React, { Component } from 'react';
+import {connect} from "react-redux";
 
 
 class App extends Component {
+	state = {
+		searchInput: "",
+	}
+
+	submitSearch = (event) => {
+		event.preventDefault()
+		this.props.dispatch({type: "FETCH_GIPHY_GIFS", payload: this.state.searchInput});
+		this.setState({searchInput: ""});
+	}
 
   render() {
     return (
       <div>
         <h1>Giphy Search!</h1>
-      </div>
+				<form onSubmit={this.submitSearch}>
+				<input type="text" value={this.state.searchInput} onChange={(event) => this.setState({searchInput: event.target.value})}/>
+				<button type="submit">Submit</button>
+				</form>
+			</div>
     );
   }
   
 }
 
-export default App;
+export default connect()(App);
--- src/index.js
+++ src/index.js
@@ -6,6 +6,36 @@ import { Provider } from 'react-redux';
 import createSagaMiddleware from 'redux-saga';
 import { takeEvery, put } from 'redux-saga/effects';
 import axios from 'axios';
+import logger from "redux-logger";
+
+//saga for getting GIfs from giphy
+function* fetchGiphyGifs (action) {
+    //wrap it in a try/catch
+		//yield axios
+		// api key: eRkQ774YosOSGmThbU2GmO421oAsXmyV
+    try{
+        const response = yield axios.get(`https://api.giphy.com/v1/gifs/search?api_key=su7x5YLWWje688tDfOceZmehnGWnCWcJ&q=${action.payload}&limit=25&offset=0&rating=G&lang=en`);
+				console.log(response.data);
+				yield put({type: "SET_GIPHY_GIFS", payload: response.data})
+    }catch(error){
+			console.log(error);
+        alert('Unable to get Gifs from Giphy')
+    }
+
+}
+
+// GET on /favorite
+function* fetchFavGifs(action) {
+  // wrap it all in try/catch
+  // yield axios
+  // dispatch the result with put!
+  try {
+    const response = yield axios.get("/favorite");
+    yield put({ type: "SET_FAV_GIFS", payload: response.data });
+  } catch (error) {
+    console.log('Error getting favorites ', error);
+  }
+}// end GET
 
 // sagas
     // rootsaga
@@ -18,6 +48,10 @@ import axios from 'axios';
         // - FETCH_FAV_GIFS (search string payload)
         //	- query database
         //  - dispatch SET_FAV_GIFS (response)
+function* rootSaga()   {
+    yield takeEvery('FETCH_GIPHY_GIFS', fetchGiphyGifs);
+    yield takeEvery('FETCH_FAV_GIFS', fetchFavGifs);
+} 
 
 // redux store
 // giphyReducer (contain all results from search
@@ -25,4 +59,41 @@ import axios from 'axios';
 // favoritesReducer (contains all favorites)
 // - SET_FAV_GIFS
 
-ReactDOM.render(<App />, document.getElementById('react-root'));
+// giphyReducer
+const giphyReducer = (state = [], action) => {
+  switch (action.type) {
+    case 'SET_GIPHY_GIFS':
+      return [...action.payload.data ]
+    default:
+      return state;
+  }
+};
+
+// favoritesReducer
+const favoritesReducer = (state = [], action) => {
+  switch (action.type) {
+    case 'SET_FAV_GIFS':
+      return [...action.payload ]
+    default:
+      return state;
+  }
+};
+
+// Create sagaMiddleware
+const sagaMiddleware = createSagaMiddleware();
+
+// Create one store that all components can use
+const storeInstance = createStore(
+    combineReducers({
+        giphyReducer,
+        favoritesReducer
+    }),
+    // Add sagaMiddleware to our store
+    applyMiddleware(sagaMiddleware, logger),
+);
+
+// Pass rootSaga into our sagaMiddleware
+sagaMiddleware.run(rootSaga);
+
+ReactDOM.render(<Provider store={storeInstance}><App /></Provider>, 
+    document.getElementById('react-root'));
