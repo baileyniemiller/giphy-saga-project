37d2e9853375b2e7ba51a639b7f02f3584808787
its-justus <ianjohnson9042@gmail.com>
2020-06-26 08:31:52 -0500

    tile bar working

--- src/components/Gif/Gif.js
+++ src/components/Gif/Gif.js
@@ -2,13 +2,39 @@ import React, { Component } from "react";
 import { connect } from "react-redux";
 import GridListTile from "@material-ui/core/GridListTile";
 import GridListTileBar from "@material-ui/core/GridListTileBar";
+import IconButton from "@material-ui/core/IconButton";
+import FavoriteBorderIcon from "@material-ui/icons/FavoriteBorder";
 
 class Gif extends Component {
+  state = {
+    showTileBar: false,
+  };
+
+  // onMouseEnter={() => setIsShown(true)}
+  //onMouseLeave={() => setIsShown(false)}>
+
   render() {
-		const {gif} = this.props;
+    const { gif } = this.props;
     return (
-      <GridListTile key={gif.img} cols={gif.cols || 1}>
+      <GridListTile
+        key={gif.img}
+        cols={gif.cols || 1}
+        onMouseEnter={() => this.setState({ showTileBar: true })}
+        onMouseLeave={() => this.setState({ showTileBar: false })}
+      >
         <img src={gif.url} alt={gif.title} />
+        {this.state.showTileBar && (
+          <GridListTileBar
+            title={gif.title}
+            actionIcon={
+              <IconButton aria-label={`info about ${gif.title}`}>
+                <FavoriteBorderIcon color="secondary" />
+              </IconButton>
+            }
+          >
+            test
+          </GridListTileBar>
+        )}
       </GridListTile>
     );
   }
